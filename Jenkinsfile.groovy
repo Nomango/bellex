465b85df840af15ad16b0712832ecb9fb26d2706
Nomango <569629550@qq.com>
2019-03-05 01:45:06 +0800

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,28 +13,28 @@ pipeline {
                 }
             }
         }
-        // stage('Build Bellex Back-end') {
-        //     steps {
-        //         dir('C:/Users/Administrator/go/src/github.com/nomango/bellex/server') {
-        //             bat 'go build -o bellex main.go'
-        //         }
-        //     }
-        // }
-    }
-    post {
-        success {
-            emailext body:
-                '''${SCRIPT, template="build.template"}''',
-                subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is success :)",
-                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-                to: "569629550@qq.com"
-        }
-        failure {
-            emailext body:
-                '''${SCRIPT, template="build.template"}''',
-                subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is failed :(",
-                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-                to: "569629550@qq.com"
+        stage('Build Bellex Back-end') {
+            steps {
+                dir('C:/Users/Administrator/go/src/github.com/nomango/bellex/server') {
+                    bat 'go build -o bellex main.go'
+                }
+            }
         }
     }
+    // post {
+    //     success {
+    //         emailext body:
+    //             '''${SCRIPT, template="build.template"}''',
+    //             subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is success :)",
+    //             recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+    //             to: "569629550@qq.com"
+    //     }
+    //     failure {
+    //         emailext body:
+    //             '''${SCRIPT, template="build.template"}''',
+    //             subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is failed :(",
+    //             recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+    //             to: "569629550@qq.com"
+    //     }
+    // }
 }

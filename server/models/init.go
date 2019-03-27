7f624a6a822a4ba9e97fb80e44aa21b81e59535c
Nomango <569629550@qq.com>
2019-03-28 00:41:41 +0800

    update

--- server/models/init.go
+++ server/models/init.go
@@ -1,13 +1,19 @@
 package models
 
 import (
+	"log"
+
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "bellex:Bellex2019@/bellex?charset=utf8")
+	if err := orm.RegisterDriver("mysql", orm.DRMySQL); err != nil {
+		log.Fatalln("Register driver failed", err)
+	}
+	if err := orm.RegisterDataBase("default", "mysql", "bellex:Bellex2019@/bellex?charset=utf8"); err != nil {
+		log.Fatalln("Register database failed", err)
+	}
 
 	orm.RegisterModel(new(Bell))
 }
